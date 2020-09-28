c88658594b9e015f38491bdf9415597dde951562
Ankit Kumar <ankit@ubiquity6.com>
2020-09-27 23:52:59 -0700

    push

---
+++ download.sh
@@ -0,0 +1,6 @@
+curl https://composer-assets.s3.amazonaws.com/studioSkybox/px.jpg --output px.jpg
+curl https://composer-assets.s3.amazonaws.com/studioSkybox/py.jpg --output py.jpg
+curl https://composer-assets.s3.amazonaws.com/studioSkybox/pz.jpg --output pz.jpg
+curl https://composer-assets.s3.amazonaws.com/studioSkybox/nx.jpg --output nx.jpg
+curl https://composer-assets.s3.amazonaws.com/studioSkybox/ny.jpg --output ny.jpg
+curl https://composer-assets.s3.amazonaws.com/studioSkybox/nz.jpg --output nz.jpg
